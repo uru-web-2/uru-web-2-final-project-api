f62c517568d829fc9d288e1e39f75a3e0bbd0132
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-04-01 17:18:17 -0400

    refactor: modified document-related validation models

--- src/router/Library/Document/Magazine/MagazineIssueModel.js
+++ src/router/Library/Document/Magazine/MagazineIssueModel.js
@@ -4,10 +4,10 @@ import {REMOVE_ARTICLE} from "../Article/ArticleModel.js";
 // Create magazine issue model
 export const CREATE_MAGAZINE_ISSUE = Joi.object({
     document_title: Joi.string().required().min(1).max(255),
-    document_description: Joi.string().required().min(1).max(255),
+    document_description: Joi.string().required().min(1).max(4000),
     document_release_date: Joi.date().required(),
     document_pages: Joi.number().required().min(1),
-    document_author: Joi.string().required().min(1).max(255),
+    document_author: Joi.array().items(Joi.string().min(1).max(255)),
     document_topic_ids: Joi.array().items(Joi.number().required().min(1)).required(),
     document_location_section_ids: Joi.array().items(Joi.number().required().min(1)).required(),
     document_language_ids: Joi.array().items(Joi.number().required().min(1)).required(),
@@ -19,10 +19,10 @@ export const CREATE_MAGAZINE_ISSUE = Joi.object({
 export const UPDATE_MAGAZINE = Joi.object({
     id: Joi.number().required().min(1),
     document_title: Joi.string().required().min(1).max(255),
-    document_description: Joi.string().required().min(1).max(255),
+    document_description: Joi.string().required().min(1).max(4000),
     document_release_date: Joi.date().required(),
     document_pages: Joi.number().required().min(1),
-    document_author: Joi.string().required().min(1).max(255),
+    document_author: Joi.array().items(Joi.string().min(1).max(255)),
     document_create_document_topic_ids: Joi.array().items(Joi.number().min(1)),
     document_remove_document_topic_ids: Joi.array().items(Joi.number().min(1)),
     document_create_document_location_section_ids: Joi.array().items(Joi.number().min(1)),
